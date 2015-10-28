360b8e0ba65745180a73f401ef4a34c4491a0503
susu <u1240976@gmail.com>
2015-10-29 02:34:15 +0800

    [triangle] implement feature code, pass all Weak Robust test

--- triangle.cpp
+++ triangle.cpp
@@ -13,7 +13,34 @@ enum TriangleType {
 
 // 0 < a, b, c < 200 and a < b+c and b < a+c and c < a+b
 TriangleType check_triangle_type(int a, int b, int c){
-    return TRI_INVALID;
+    if( a <= 0 || a >= 200 )
+        return TRI_INVALID;
+    if( b <= 0 || b >= 200 )
+        return TRI_INVALID;
+    if( c <= 0 || c >= 200 )
+        return TRI_INVALID;
+    
+    if( a >= b+c )
+        return TRI_INVALID;
+    if( b >= a+c )
+        return TRI_INVALID;
+    if( c >= a+b )
+        return TRI_INVALID;
+
+    int side_equal = 0;
+    if( a == b )
+        side_equal += 1;
+    if( b == c )
+        side_equal += 1;
+    if( a == c )
+        side_equal += 1;
+
+    if( side_equal == 3 )
+        return TRI_EQUILATERAL;
+    if( side_equal == 1 )
+        return TRI_ISOSCELES;
+
+    return TRI_SCALENE;
 }
 
 void example_usage(){
